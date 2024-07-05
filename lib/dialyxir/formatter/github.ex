17fabb8f9c25db76c10243adc9821e1b6af6b6ca
carl <carl@sendle.com>
2024-07-05 12:15:21 +1000

    Build file path in umbrella apps

--- lib/dialyxir/formatter/github.ex
+++ lib/dialyxir/formatter/github.ex
@@ -7,17 +7,34 @@ defmodule Dialyxir.Formatter.Github do
 
   @impl Dialyxir.Formatter
   def format({_tag, {file, location}, {warning_name, arguments}}) do
-    base_name = Path.relative_to_cwd(file)
+    base_name = get_path(file)
 
     warning = Utils.warning(warning_name)
     string = warning.format_short(arguments)
 
     case location do
       {line, col} ->
-        "::warning file=#{base_name},line=#{line},col=#{col},title=#{warning_name}::#{string}"
+        "::warning file=#{base_name},startLine=#{line},startColumn=#{col},title=#{warning_name}::#{string}"
 
       line ->
-        "::warning file=#{base_name},line=#{line},title=#{warning_name}::#{string}"
+        "::warning file=#{base_name},startLine=#{line},title=#{warning_name}::#{string}"
     end
+  end
+
+  defp get_path(file) do
+    if File.exists?(file) do
+      file
+    else
+      cwd = File.cwd!()
+      case Path.wildcard("#{cwd}/**/#{file}") do
+        [path] ->
+          Path.relative_to_cwd(path)
+        _ ->
+          file
+      end
+    end
+  rescue
+    File.Error ->
+      file
   end
 end
